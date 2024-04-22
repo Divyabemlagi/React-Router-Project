6a9e3cbed6328207175629d123876022fdbfe969
Divyabemlagi <93431309+Divyabemlagi@users.noreply.github.com>
2024-04-22 23:58:53 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,26 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import Page from "./Page";
+import Home from "./Home";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App />}>
+      <Route path="page" element={<Page />} />
+      <Route path="home" element={<Home />} />
+    </Route>
+  )
+);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
